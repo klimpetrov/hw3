00eac28eeffb681672b6ffce520775a8f5415d8e
Klim Petrov <klim.petrov@hhu.com>
2015-11-15 22:14:38 +0100

    Not sure if it's working properly

--- statistics.cxx
+++ statistics.cxx
@@ -1,21 +1,38 @@
 #include <cmath>
 #include <iostream>
 #include <cstdlib>
-
+#include <ctime>
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
+//---------------------------------------------
+void fill(double*p,const int N,double a,double b, double r){
+	for (int i=0;i<N;i++){
+  b = RAND_MAX;
+  a = rand();
+  r = a/b;
+	p[i]=r;}
+}
+void zaehle(const int N, double*p,double& mean,double& var){	
+	for (int i=0;i<N;i++){	
+		mean += p[i]*(1/N);
+		var += pow(p[i]-mean,2)*(1/N);}
+	}
 //--------------------------------------------------
 
 int main(){
+   srand(time(NULL));
    const int N = 100;
    double p[N];
    double mean, var;
+   double b ;
+   double a ;
+   double r ;
+fill(p,N,a,b,r);
+zaehle(N,p,mean,var);
 
    // Some lines here....
 
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
-
+   
    return 0;
 }
